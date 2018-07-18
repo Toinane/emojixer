c744a94af7d625729d08754a5e183f438d4f7fdb
Toinane <toinane@crea-that.fr>
2018-07-18 17:54:18 +0200

    update emojis

--- index.js
+++ index.js
@@ -1,65 +1,53 @@
 let assets = {
-  body: 11,
-  browLeft: 18,
-  browRight: 18,
-  cheekLeft: 5,
-  cheekRight: 5,
-  eyeLeft: 38,
-  eyeRight: 38,
-  foreground: 5,
-  head: 25,
-  mouth: 35
+  twemoji: 197
 }
+let menu = [];
+let order = document.querySelector('#order');
 
-function getUri (id) {
-  if(id.replace(/Left/, 's/left/')) id = id.replace(/Left/, 's/left');
-  if(id.replace(/Right/, 's/right/')) id = id.replace(/Right/, 's/right');
+let sortable = Sortable.create(order, {
+  handle: ".handle",
+  onSort: function (event) {
+    console.log(event);
+  }
+});
 
-  return id;
-}
 
 function initListener () {
   let images = document.querySelectorAll('#menu div');
 
   for(let image of images) {
     image.addEventListener('click', function(event) {
-      let img = new Image();
-      let uri = getUri(this.classList.value);
-      let id = this.getAttribute('value');
-
-      console.log(`assets/twemoji/${uri}/${id}.svg`)
-      
-      img.src = `assets/twemoji/${uri}/${id}.svg`;
-      img.onload = function() {
-        let canvas = document.querySelector('#canvas');
-        let ctx = canvas.getContext('2d');
-
-        ctx.drawImage(img, 0, 0);
-      }
+      order.insertBefore(this.cloneNode(), order.firstChild);
     })
   }
 }
 
-function initCanvas () {
-  
+function initCanvas (el) {
+  let img = new Image();
+  let id = el.getAttribute('value');
 
+  img.src = `assets/twemoji/${id}.svg`;
+  img.onload = function() {
+    let canvas = document.querySelector('#canvas');
+    let ctx = canvas.getContext('2d');
+
+    ctx.drawImage(img, 0, 0);
+  }
 }
 
 function createMenu () {
   let menu = document.querySelector('#menu');
   let html = '';
+  let background = '';
 
-  for(let part in assets) {
-    // html += `<p>${part}</p>`;
-    for(let i = 1; i <= assets[part]; i++) {
-      html += `<div class="${part}" value="${i}" style="background-image: url('assets/twemoji/${getUri(part)}/${i}.svg');"></div>`;
-    }
+  for(let i = 1; i <= assets.twemoji; i++) {
+    if(i > 11) background = 'yellow';
+    html += `<div class="${background}" value="${i}" style="background-image: url('assets/twemoji/${i}.svg');"></div>`;
   }
   
   menu.innerHTML = html;
 
   initListener();
-  initCanvas()
 }
 
 createMenu();
